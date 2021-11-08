e3f8ef68fb72c919406f0f1f891f2f0382e1adaf
Byanka Partlow <byanka.partlow@gmail.com>
2021-11-08 13:06:25 -0800

    PyPoll_Analysis

--- PyPoll.py
+++ PyPoll.py
@@ -1,3 +1,5 @@
+import csv
+import os
 # Import the datetime class from the datetime module.
 import datetime
 # Use the now() attribute on the datetime class to get the present time.
@@ -5,14 +7,25 @@ now = datetime.datetime.now()
 # Print the present time.
 print("The time right now is ", now)
 
-#open results
-# file_to_load = open('Resources/election_results.csv', 'r')
+# open results
+file_to_load = os.path.join('Resources/election_results.csv', 'r')
 # Open the election results and read the file
 with open('Resources/election_results.csv', 'r') as election_data:
 
      # To do: perform analysis.
      print(election_data)
 
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.RFT")
+# Using the with statement open the file as a text file.
+outfile = open("analysis", "election_analysis.RFT", "w")
+# Using the open() function with the "w" mode we will write data to the file.
+open("analysis", "election_analysis.RFT", "w")
+# Write some data to the file.
+outfile.write("Hello World")
+
+# Close the file
+outfile.close()
 
 # To do: perform analysis.
 
