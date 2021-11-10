be541ff7f82b737731e4e2aca11b90ab021a5bac
Byanka Partlow <byanka.partlow@gmail.com>
2021-11-09 19:04:55 -0800

    PyPoll

--- PyPoll.py
+++ PyPoll.py
@@ -1,40 +1,53 @@
+# Add our dependencies.
 import csv
 import os
-# Import the datetime class from the datetime module.
-import datetime
-# Use the now() attribute on the datetime class to get the present time.
-now = datetime.datetime.now()
-# Print the present time.
-print("The time right now is ", now)
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("Resources","analysis", "election_analysis.txt")
 
-# open results
-file_to_load = os.path.join('Resources/election_results.csv', 'r')
-# Open the election results and read the file
-with open('Resources/election_results.csv', 'r') as election_data:
+# 1. Initialize a total vote counter.
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+# To do: read and analyze the data here.
+     # Read the file object with the reader function.
+     file_reader = csv.reader(election_data)
+# Print the header row.
+     headers = next(file_reader)
+   # # Print each row in the CSV file.
+     for row in file_reader:
+          #add total vote count
+          total_votes += 1
+          #print candidate name from each row
+          candidate_name = row[2]
+          # If the candidate does not match any existing candidate...
+          if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+               candidate_options.append(candidate_name)
+                # 2. Begin tracking that candidate's vote count
+               # Begin tracking that candidate's vote count.
+               candidate_votes[candidate_name] = 0
 
-     # To do: perform analysis.
-     print(election_data)
+        # Add a vote to that candidate's count
+          candidate_votes[candidate_name] += 1
+     # Determine the percentage of votes for each candidate by looping through the counts.
+     for candidate_name in candidate_votes:
+          #retrieve vote count of candidate
+          votes = candidate_votes[candidate_name]
+          #percent of votes
+          vote_percentage = float(votes) / float(total_votes) * 100
+          print(f'{candidate_name}: received {vote_percentage}% of the vote.')
+print(total_votes)
+print(candidate_options)
+print(candidate_votes)
+# # Close the file.
+election_data.close()
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.RFT")
-# Using the with statement open the file as a text file.
-outfile = open("analysis", "election_analysis.RFT", "w")
-# Using the open() function with the "w" mode we will write data to the file.
-open("analysis", "election_analysis.RFT", "w")
-# Write some data to the file.
-outfile.write("Hello World")
-
-# Close the file
-outfile.close()
-
-# To do: perform analysis.
-
-# Close the file.
-
-
-# The data we need to retrieve
-#1. The total number of votes cast
 #2. A complete list of candidates who received votes
+
 #3. Percent voates each candidate won.
 #4. Total number of votes each candidate won
 # 5. Winner of election
